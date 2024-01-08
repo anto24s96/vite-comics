e4704991e45b5870842773a6a071028733f4f82f
anto24s96 <anto24s96@gmail.com>
2024-01-08 16:59:57 +0100

    created main section

---
+++ src/components/AppMain.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "AppMain",
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<template lang="">
+    <div id="bg_black">
+        <main class="container">
+            <h2 class="text-white">--Qui il content--</h2>
+        </main>
+    </div>
+</template>
+<style lang="scss" scoped>
+#bg_black {
+    background-color: black;
+
+    main {
+        font-size: 30px;
+
+        h2 {
+            padding: 50px 0;
+        }
+    }
+}
+</style>
