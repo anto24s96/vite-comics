d4b0c56a076490672dea0f66f7a22af692f62d96
anto24s96 <anto24s96@gmail.com>
2024-01-09 18:14:07 +0100

    added AppCovers component

---
+++ src/components/AppCovers.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: "AppCovers",
+    props: {
+        cover: Object
+    }
+}
+</script>
+<template lang="">
+    <div class="card d-flex align-items">
+        <div class="cover_container">
+            <img :src="`${cover.thumb}`" alt="photos">
+        </div>
+        <span class="cover_title text-white">{{ cover.series }}</span>
+    </div> 
+</template>
+<style lang="scss" scoped>
+.card {
+    margin: 10px;
+    width: calc(100% / 6 - 20px);
+    flex-direction: column;
+    padding: px 0;
+
+    .cover_container {
+        height: 200px;
+        width: 100%;
+
+        img {
+            height: 200px;
+            object-fit: cover;
+            object-position: top;
+        }
+
+    }
+
+    .cover_title {
+        text-transform: uppercase;
+        display: inline-block;
+        padding: 10px 0;
+        font-weight: 600;
+        text-align: center;
+    }
+}
+</style>
