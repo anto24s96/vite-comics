c6018d3ad4ed1591048e361cfd8352166d4632d2
anto24s96 <anto24s96@gmail.com>
2024-01-09 16:51:46 +0100

    added current series button in AppHero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -5,14 +5,30 @@ export default {
 </script>
 <template lang="">
   <div class="banner_container">
-    
+    <div class="container">
+        <span class="current_series text-white">current series</span>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
 .banner_container {
     background-image: url(../assets/img/jumbotron.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     height: 400px;
+    position: relative;
+
+    .current_series {
+        display: inline-block;
+        background-color: $blue_primary;
+        padding: 10px 30px;
+        font-size: 18px;
+        text-transform: uppercase;
+        font-weight: 800;
+        position: absolute;
+        bottom: -18px;
+    }
 }
 </style>
